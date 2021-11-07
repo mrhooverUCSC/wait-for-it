547772f3bb113fe08ba1cc866f6d83da7e7f018c
jnguy182 <63328580+jnguy182@users.noreply.github.com>
2021-11-07 15:45:41 -0800

    Jake: clean up background assets

--- src/parallax.js
+++ src/parallax.js
@@ -1,13 +1,15 @@
 const canvas = document.getElementById('parallax');
 const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 1920;
-const CANVAS_HEIGHT = canvas.height = 1080;
+const CANVAS_HEIGHT = canvas.height = 1280;
 let gameSpeed = 1;
 
 const spaceBackground = new Image();
 spaceBackground.src = './assets/imgs/space.png';
-const foreground = new Image();
-foreground.src = './assets/imgs/U2O4xq8.png'
+const background = new Image();
+background.src = './assets/imgs/background.png';
+const counter = new Image();
+counter.src = './assets/imgs/counter.png';
 
 
 let x = 0;
@@ -17,7 +19,8 @@ function animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
     ctx.drawImage(spaceBackground, x, 0);
     ctx.drawImage(spaceBackground, x2, 0);
-    ctx.drawImage(foreground, 0, 0);
+    ctx.drawImage(background, 0, 0);
+    ctx.drawImage(counter, 0, 0);
     if (x < -1920) x = 1920 + x2 - gameSpeed;
     else x -= gameSpeed;
     if (x2 < -1920) x2 = 1920 + x - gameSpeed;
