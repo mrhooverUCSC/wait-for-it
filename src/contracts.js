56085be0ad518d62f16bc18f259bf7fdba727308
KristopherYu <74690002+KristopherYu@users.noreply.github.com>
2021-11-07 13:45:31 -0800

    Added percent of credits to beans rather than fixed amount.

--- src/contracts.js
+++ src/contracts.js
@@ -1,12 +1,28 @@
 let beansContractCurrently = false;
+let currentContractUpgrade = 0;
+let contractRate = 0.05;
+let amountToUpgrade = 0.03;
 let timer = 0;
 
 function beansContract(){
-    if(credits >= 500){
-        increaseCredits(-500);
-        document.getElementById("beansContract").hidden = true;
+    var contractCost = Math.floor(500 + (100 * (currentContractUpgrade)));
+    if(currentContractUpgrade > 0){
+        var contractCost = Math.floor(500 + (100 * (currentContractUpgrade)));  
+        if(credits >= contractCost){
+            increaseCredits(-contractCost);
+            document.getElementById("beansContract").innerHTML = "Upgrade Beans Contract (" + contractCost + " credits)";
+            currentContractUpgrade++;
+            contractRate = contractRate + amountToUpgrade;
+            amountToUpgrade = amountToUpgrade * 0.9;
+            document.getElementById("beansContractText").innerHTML = "Beans Contract turns " + Math.floor(contractRate * 100) + "% credits into beans every 15 seconds)";
+        }
+    }
+    else if(credits >= 250 && !beansContractCurrently && currentContractUpgrade < 1){
+        increaseCredits(-250);
+        //document.getElementById("beansContract").hidden = true;
+        document.getElementById("beansContract").innerHTML = "Upgrade Beans Contract (" + contractCost + " credits)";
         document.getElementById("beansContractToggle").hidden = false;
-        document.getElementById("contract").hidden = false;
+        currentContractUpgrade++;
     }
 }
 
@@ -24,14 +40,8 @@ function contractsTick(){
     timer += .1; 
     if(timer >= 15){
         if(beansContractCurrently){
-            if(credits > 10){
-                increaseCredits(-10);
-                increaseBeans(10);
-            }
-            else{
-                increaseBeans(credits);
-                increaseCredits(-credits);
-            }
+            increaseBeans(Math.floor(contractRate * credits));
+            increaseCredits(-Math.floor(contractRate * credits));
         }
         timer = 0;
     }
