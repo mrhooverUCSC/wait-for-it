ab7d70f7ca43301fcef9c2ac83cc39088d2abe09
mrhooverUCSC <61717155+mrhooverUCSC@users.noreply.github.com>
2021-11-07 16:20:22 -0800

    Fixed scaling of beans contract

--- src/contracts.js
+++ src/contracts.js
@@ -5,15 +5,16 @@ let amountToUpgrade = 0.03;
 let timer = 0;
 
 function beansContract(){
-    var contractCost = Math.floor(500 + (100 * (currentContractUpgrade)));
+    var contractCost = Math.floor(500 + (100 * (currentContractUpgrade)));  
     if(credits >= contractCost){
+        document.getElementById("beansContractToggle").hidden = false;
+        document.getElementById("beansContractText").innerHTML = "Beans Contract currently turns " + Math.floor(contractRate * 100) + "% credits of into beans every 15 seconds)";
         increaseCredits(-contractCost);
         currentContractUpgrade++;
         contractRate = contractRate + amountToUpgrade;
-        amountToUpgrade = amountToUpgrade * 0.9;
+        amountToUpgrade = amountToUpgrade * 0.85;
         contractCost = Math.floor(500 + (100 * (currentContractUpgrade)));
         document.getElementById("beansContract").innerHTML = "Upgrade Beans Contract (" + contractCost + " credits)";
-        document.getElementById("beansContractText").innerHTML = "Beans Contract turns " + Math.floor(contractRate * 100) + "% credits into beans every 15 seconds)";
     }
 }
 
